f06fc0e7570a9a5ecbefd2147cb53666af867403
Justin Rieko Tolentino <jstolentino2@student.hau.edu.ph>
2024-10-30 17:12:38 +0800

    try

--- CpE201/settings.py
+++ CpE201/settings.py
@@ -61,6 +61,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
